2308b422ba957664a1dcbb47819120b92d9d27fd
テレンス ・ マイハンド <x977@hotmail.com>
2016-09-08 16:05:01 -0400

    second commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+string firstName;
+string middleinitial;
+string lastName;
+string fullName;
