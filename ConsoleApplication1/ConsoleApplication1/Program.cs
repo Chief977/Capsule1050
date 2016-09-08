089861be9c8acd3be3e96e2ee2c2ae3e36318a88
テレンス ・ マイハンド <x977@hotmail.com>
2016-09-08 16:31:14 -0400

    4th commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,18 +3,19 @@
     class Program
     {
         static void Main(string[] args)
+
         {
+            string firstName;
+            string middleinitial;
+            string lastName;
+            string fullName;
+            int age;
+            bool isCitizen;
+            bool canVote;
+            int heightFeet;
+            double heightInches;
+            double totalHeightCM;
         }
     }
 }
 
-string firstName;
-string middleinitial;
-string lastName;
-string fullName;
-int age;
-bool isCitizen;
-bool canVote;
-int heightFeet;
-double heightInches;
-double totalHeightCM;
