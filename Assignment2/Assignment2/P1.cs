7c377aad2efdd9f68317d9f1c255bbe68d2ec129
テレンス ・ マイハンド <x977@hotmail.com>
2016-09-23 18:29:44 -0400

    Assignment 2 commit 1

---
+++ Assignment2/Assignment2/P1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class P1
+    {
+        public int age;
+        public string firstName;
+        public string lastName;
+        public P1 spouse;
+
+    }
+}
---
+++ Assignment2/Assignment2/P2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class P2
+    {
+        public int age;
+        public string firstName;
+        public string lastName;
+        public P2 spouse;
+    }
+}
